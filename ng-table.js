b011cff4364e13ac0cb41aaebe214ec9707e80a1
jburrow <jaburrow@gmail.com>
2014-08-13 19:17:58 +0100

    Updated demo4 to have text and numbers

--- ng-table.js
+++ ng-table.js
@@ -708,7 +708,7 @@ app.directive('ngTablePagination', ['$compile',
 ]);
 
 angular.module('ngTable').run(['$templateCache', function ($templateCache) {
-	$templateCache.put('ng-table/filters/number.html', '<form><input type="number" name="number" ng-pattern="/^\D+$/" ng-model="params.filter()[name]" ng-if="filter==\'number\'" class="input-filter form-control"/><form> ');
+	$templateCache.put('ng-table/filters/number.html', '<form name="{{column.filterName}}"><input type="number" name="{{column.filterName}}" ng-model="params.filter()[name]" ng-if="filter==\'number\'" class="input-filter form-control"/> <form> ');
 	$templateCache.put('ng-table/filters/select-multiple.html', '<select ng-options="data.id as data.title for data in column.data" multiple ng-multiple="true" ng-model="params.filter()[name]" ng-show="filter==\'select-multiple\'" class="filter filter-select-multiple form-control" name="{{column.filterName}}"> </select>');
 	$templateCache.put('ng-table/filters/select.html', '<select ng-options="data.id as data.title for data in column.data" ng-model="params.filter()[name]" ng-show="filter==\'select\'" class="filter filter-select form-control" name="{{column.filterName}}"> </select>');
 	$templateCache.put('ng-table/filters/text.html', '<input type="text" name="{{column.filterName}}" ng-model="params.filter()[name]" ng-if="filter==\'text\'" class="input-filter form-control"/>');
