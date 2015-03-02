01b4c4775d336282421a53571798c855250b8836
Bruno Oliveira <321.bruno@gmail.com>
2015-03-02 11:30:55 -0300

    Scroll Infinite in ngTable
    Implementation de scrolling infinite in ngTable. Not jQuery or plugins,
    native AngularJS.

--- dist/ng-table.js
+++ dist/ng-table.js
@@ -483,7 +483,8 @@ app.factory('NgTableParams', ['$q', '$log', 'ngTableDefaults', function($q, $log
             counts: [10, 25, 50, 100],
             sortingIndicator: 'span',
             getGroups: this.getGroups,
-            getData: this.getData
+            getData: this.getData,
+            isPagination: true
         };
         angular.extend(settings, ngTableDefaults.settings);
 
@@ -950,6 +951,9 @@ app.directive('ngTablePagination', ['$compile',
                     if (angular.isUndefined(templateUrl)) {
                         return;
                     }
+					if(!scope.params.settings().isPagination){
+                        return;
+                    }
                     var template = angular.element(document.createElement('div'))
                     template.attr({
                         'ng-include': 'templateUrl'
@@ -961,6 +965,70 @@ app.directive('ngTablePagination', ['$compile',
         };
     }
 ]);
+app.directive('ngTablePaginationScroll', [
+    '$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout) {
+    'use strict';
+    return {
+        restrict: 'A',
+        scope: true,
+        link: function(scope, elem, attrs){
+            var checkWhenEnabled, handler, scrollDistance, scrollEnabled;
+            $window = angular.element($window);
+            scrollDistance = 0;
+            if (attrs.infiniteScrollDistance != null) {
+                scope.$watch(attrs.infiniteScrollDistance, function(value) {
+                    return scrollDistance = parseInt(value, 10);
+                });
+            }
+            scrollEnabled = true;
+            checkWhenEnabled = false;
+            if (attrs.infiniteScrollDisabled != null) {
+                scope.$watch(attrs.infiniteScrollDisabled, function(value) {
+                    scrollEnabled = !value;
+                    if (scrollEnabled && checkWhenEnabled) {
+                        checkWhenEnabled = false;
+                        return handler();
+                    }
+                });
+            }
+            handler = function() {
+                var elementBottom, remaining, shouldScroll, windowBottom, documentInfo;
+                documentInfo = $window[0].document.body;
+                windowBottom = document.documentElement.clientHeight + documentInfo.scrollTop;
+                elementBottom = elem[0].offsetTop + elem[0].scrollHeight;
+                remaining = elementBottom - windowBottom;
+                shouldScroll = remaining <= document.documentElement.clientHeight * scrollDistance;
+                if (shouldScroll && scrollEnabled && documentInfo.scrollTop) {
+                    var total = scope.params.total();
+                    var qtyItems = scope.params.data.length;
+                    var qtyPages = Math.ceil(total / scope.params.count());
+                    var pageCurrent = scope.params.page();
+                    var pageNew = pageCurrent + 1;
+                    if(qtyItems<=total && pageNew<=qtyPages){
+                            scope.params.page( scope.params.page() + 1 );
+                            scope.$apply(scope);
+                    }
+                } else if (shouldScroll) {
+                    return checkWhenEnabled = true;
+                }
+            };
+            var promise = null;
+            $window.on('scroll', handler);
+            scope.$on('$destroy', function() {
+                return $window.off('scroll', handler);
+            });
+            return $timeout((function() {
+                if (attrs.infiniteScrollImmediateCheck) {
+                    if (scope.$eval(attrs.infiniteScrollImmediateCheck)) {
+                        return handler();
+                    }
+                } else {
+                    return handler();
+                }
+            }), 300);
+        }
+    };
+}]);
 angular.module('ngTable').run(['$templateCache', function ($templateCache) {
 	$templateCache.put('ng-table/filters/select-multiple.html', '<select ng-options="data.id as data.title for data in $column.data" ng-disabled="$filterRow.disabled" multiple ng-multiple="true" ng-model="params.filter()[name]" ng-show="filter==\'select-multiple\'" class="filter filter-select-multiple form-control" name="{{name}}"> </select>');
 	$templateCache.put('ng-table/filters/select.html', '<select ng-options="data.id as data.title for data in $column.data" ng-disabled="$filterRow.disabled" ng-model="params.filter()[name]" ng-show="filter==\'select\'" class="filter filter-select form-control" name="{{name}}"> </select>');
