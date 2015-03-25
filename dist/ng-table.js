51a4a94a959d18ffb9233d480673bba147a38079
Adam Miles <adammiles10@gmail.com>
2015-03-25 17:52:09 -0400

    issue with trying to load angular before it's required by requirejs.
    changing to use window.angular to avoid this error

--- dist/ng-table.js
+++ dist/ng-table.js
@@ -8,7 +8,7 @@
     } else {
         return factory(angular);
     }
-}(angular || null, function(angular) {
+}(window.angular || null, function(angular) {
     'use strict';
 /**
  * ngTable: Table + Angular JS
