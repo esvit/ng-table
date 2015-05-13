17f15b624561d12048a727606b72458227afa82d
ccrowhurstram <ccrowhurst@ramplc.com>
2015-05-13 18:35:21 +0100

    docs(api-docs): add generated api docs

---
+++ api-docs/app/js/nav-service.js
@@ -0,0 +1,74 @@
+module.exports = {
+    name:'NAVSERVICE',
+    data:[
+  {
+    "title": "API Docucmentation",
+    "name": "api",
+    "url": "/docs/api",
+    "items": [
+      {
+        "type": "module",
+        "items": [
+          {
+            "name": "ngTable",
+            "url": "partials/api/ngTable/index.html"
+          }
+        ]
+      },
+      {
+        "type": "service",
+        "items": [
+          {
+            "name": "NgTableParams",
+            "url": "partials/api/ngTable/service/NgTableParams.html"
+          },
+          {
+            "name": "ngTableParams",
+            "url": "partials/api/./service/ngTableParams.html"
+          },
+          {
+            "name": "ngTableColumn",
+            "url": "partials/api/ngTable/service/ngTableColumn.html"
+          }
+        ]
+      },
+      {
+        "type": "directive",
+        "items": [
+          {
+            "name": "ngTable",
+            "url": "partials/api/ngTable/directive/ngTable.html"
+          },
+          {
+            "name": "ngTableDynamic",
+            "url": "partials/api/ngTable/directive/ngTableDynamic.html"
+          },
+          {
+            "name": "ngTablePagination",
+            "url": "partials/api/ngTable/directive/ngTablePagination.html"
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "items": [
+          {
+            "name": "ngTableDefaultParams",
+            "url": "partials/api/ngTable/object/ngTableDefaultParams.html"
+          },
+          {
+            "name": "ngTableController",
+            "url": "partials/api/./object/ngTableController.html"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "title": "Examples",
+    "name": "examples",
+    "url": "/docs/examples",
+    "items": []
+  }
+]
+};
