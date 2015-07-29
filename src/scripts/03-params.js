8d912609f156d3722bba79ea53d5232576282ae8
ccrowhurstram <ccrowhurst@ramplc.com>
2015-07-30 00:03:37 +0100

    feat(NgTableParams): better default implementation of getData that filters and sorts
    Use the ngTableDefaultGetData service to apply the `filter`, `orderBy` and paging values of the
    `NgTableParams` to the `data` array on the `settings` object

--- src/scripts/03-params.js
+++ src/scripts/03-params.js
@@ -102,7 +102,7 @@ app.value('ngTableDefaults', {
  * @description Parameters manager for ngTable
  */
 
-app.factory('NgTableParams', ['$q', '$log', 'ngTableDefaults', 'ngTableGetDataBcShim', function($q, $log, ngTableDefaults, ngTableGetDataBcShim) {
+app.factory('NgTableParams', ['$q', '$log', 'ngTableDefaults', 'ngTableGetDataBcShim', 'ngTableDefaultGetData', function($q, $log, ngTableDefaults, ngTableGetDataBcShim, ngTableDefaultGetData) {
     var isNumber = function(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     };
@@ -304,11 +304,8 @@ app.factory('NgTableParams', ['$q', '$log', 'ngTableDefaults', 'ngTableGetDataBc
          * @param {Object} params New parameters
          */
         this.getData = function(params) {
-            if (angular.isArray(this.data) && angular.isObject(params)) {
-                return this.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-            } else {
-                return [];
-            }
+            // note: this === settings
+            return ngTableDefaultGetData(this.data, params);
         };
 
         /**
