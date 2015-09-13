e7ab764075fe9a68cc0bd20e248d9681d0899d42
ccrowhurstram <ccrowhurst@ramplc.com>
2015-09-13 15:38:20 +0100

    docs(demo-site): changes to menu navigation

--- demo-site/app/app.js
+++ demo-site/app/app.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
 
-    angular.module('main', ['ngRoute', 'ngTable', 'ngSocial', 'embedCodepen'])
+    angular.module('main', ['ngRoute', 'ngTable', 'ngSocial', 'embedCodepen', 'ngSanitize'])
         .config(['$routeProvider',
             function ($routeProvider) {
                 $routeProvider.
@@ -54,6 +54,188 @@
                     element.html(hljs.highlight(scope.prettyLang || 'html', element.text()).value);
                 }
             };
+        })
+        .controller('menuController', function(){
+            var self = this;
+            self.sections = [{
+                title: 'Intro',
+                hasMore: false,
+                url: '#/',
+                items: [{
+                    title: 'Real world example',
+                    url: '#/intro/demo-real-world'
+                }]
+            }, {
+                title: 'Loading data',
+                hasMore: false,
+                items: [{
+                    title: 'Overview',
+                    url: '#/loading/overview'
+                }, {
+                    title: 'Managed array',
+                    url: '#/loading/demo-managed-array'
+                }, {
+                    title: 'Lazy loading managed array',
+                    url: '#/loading/demo-lazy-loaded'
+                }, {
+                    title: 'External array (eg server-side)',
+                    url: '#/loading/demo-external-array'
+                }]
+            }, {
+                title: 'Pagination',
+                hasMore: true,
+                items: [{
+                    title: 'Basic example',
+                    url: '#/pagination/demo-pager-basic'
+                }, {
+                    title: 'Change page / page controls programmatically',
+                    url: '#/pagination/demo-api'
+                }, {
+                    title: 'Custom template',
+                    url: '#/demo/todo'
+                }]
+            }, {
+                title: 'Sorting',
+                hasMore: true,
+                items: [{
+                    title: 'Basic example',
+                    url: '#/sorting/demo-sorting-basic'
+                }, {
+                    title: 'Change sort order programmatically',
+                    url: '#/sorting/demo-api'
+                }, {
+                    title: 'Enable sorting programmatically',
+                    url: '#/sorting/demo-enabling'
+                }]
+            }, {
+                title: 'Filtering',
+                hasMore: true,
+                items: [{
+                    title: 'Basic example',
+                    url: '#/filtering/demo-filtering-basic'
+                }, {
+                    title: 'Nested property filters',
+                    url: '#/filtering/demo-nested-property'
+                }, {
+                    title: 'Select filters',
+                    url: '#/filtering/demo-select'
+                }, {
+                    title: 'Custom filter template',
+                    url: '#/filtering/demo-custom-template'
+                }, {
+                    title: 'Multiple template filters',
+                    url: '#/filtering/demo-multi-template'
+                }, {
+                    title: 'Change filter values programmatically',
+                    url: '#/filtering/demo-api'
+                }, {
+                    title: 'Enable filters programmatically',
+                    url: '#/filtering/demo-enabling'
+                }, {
+                    title: 'Customize filter algorithm',
+                    url: '#/filtering/demo-customize-algorithm'
+                }]
+            }, {
+                title: 'Grouping',
+                hasMore: true,
+                items: [{
+                    title: 'Basic example',
+                    url: '#/grouping/demo-grouping-basic'
+                }, {
+                    title: 'Custom grouping function',
+                    url: '#/grouping/demo-grouping-fn'
+                }, {
+                    title: 'Summary row',
+                    url: '#/grouping/demo-summary'
+                }, {
+                    title: 'Change grouping programmatically',
+                    url: '#/grouping/demo-api'
+                }, {
+                    title: 'Enable grouping programmatically',
+                    url: '#/grouping/demo-enabling'
+                }]
+            }, {
+                title: 'Formatting table',
+                hasMore: true,
+                items: [{
+                    title: '<em>ngTable</em>: Data cell template',
+                    url: '#/formatting/demo-cell-values'
+                }, {
+                    title: '<em>ngTableDynamic</em>: Data cell template (via JS)',
+                    url: '#/formatting/demo-dynamic-js-values'
+                }, {
+                    title: '<em>ngTableDynamic</em>: Data cell template (via HTML)',
+                    url: '#/formatting/demo-dynamic-html-values'
+                }, {
+                    title: 'Data row template',
+                    url: '#/formatting/demo-row'
+                }, {
+                    title: 'Header cell template - basic',
+                    url: '#/formatting/demo-header-cell-basic'
+                }, {
+                    title: 'Header cell template - full',
+                    url: '#/formatting/demo-header-cell-full'
+                }, {
+                    title: 'Custom header',
+                    url: '#/formatting/demo-custom-header'
+                }]
+            }, {
+                title: 'Working with columns',
+                hasMore: false,
+                items: [{
+                    title: 'Show/hide columns',
+                    url: '#/columns/demo-visibility'
+                }, {
+                    title: 'Reorder columns',
+                    url: '#/columns/demo-reordering'
+                }]
+            }, {
+                title: 'Editing data',
+                hasMore: false,
+                items: [{
+                    title: 'Inline row edit',
+                    url: '#/editing/demo-inline'
+                }, {
+                    title: 'Batch table edit',
+                    url: '#/editing/demo-batch'
+                }]
+            }, {
+                title: 'Events',
+                hasMore: false,
+                items: [{
+                    title: 'Example',
+                    url: '#/demo/todo'
+                }]
+            }, {
+                title: 'Global customizations',
+                hasMore: false,
+                items: [{
+                    title: 'Response interceptors',
+                    url: '#/demo/todo'
+                }, {
+                    title: 'Change default parameters and settings',
+                    url: '#/demo/todo'
+                }, {
+                    title: 'Replace default filter/sorting algorithm',
+                    url: '#/demo/todo'
+                }]
+            }, {
+                title: 'Miscellaneous',
+                hasMore: false,
+                items: [{
+                    title: 'Saving params in url',
+                    url: '#/demo/todo'
+                }]
+            }, {
+                title: 'Plugins',
+                hasMore: false,
+                items: [{
+                    title: 'Resize columns',
+                    url: '#/demo/todo'
+                }, {
+                    title: 'Export to CSV',
+                    url: '#/demo/todo'
+                }]
+            }];
         });
-
 })();
