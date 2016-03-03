be3e5ffc0af45ed944f0434edffe3343a0e3508b
elvis <elvis@lnmpy.com>
2016-03-03 13:36:26 +0800

    add pagerOption

--- dist/ng-table.js
+++ dist/ng-table.js
@@ -1447,11 +1447,26 @@
                         'ng-table-pagination': 'params',
                         'template-url': 'templates.pagination'
                     });
-                    $element.after(paginationTemplate);
+                    var pagerPosition = $attrs.pagerPosition ? $attrs.pagerPosition : 'bottom';
+                    if (pagerPosition == 'bottom') {
+                        $element.after(paginationTemplate);
+                        $compile(paginationTemplate)($scope);
+                    } else if (pagerPosition == 'top') {
+                        $element.before(paginationTemplate);
+                        $compile(paginationTemplate)($scope);
+                    } else if (pagerPosition == 'both') {
+                        $element.before(paginationTemplate);
+                        $compile(paginationTemplate)($scope);
+                        var paginationTemplate = angular.element(document.createElement('div')).attr({
+                            'ng-table-pagination': 'params',
+                            'template-url': 'templates.pagination'
+                        });
+                        $element.after(paginationTemplate);
+                        $compile(paginationTemplate)($scope);
+                    }
                     if (headerTemplate) {
                         $compile(headerTemplate)($scope);
                     }
-                    $compile(paginationTemplate)($scope);
                 }
             };
 
