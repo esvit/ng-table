965845af2cca240fb0f02e63fab364d6d1be12f8
ccrowhurstram <ccrowhurst@ramplc.com>
2016-06-16 18:28:21 +0100

    chore(typings): switch from tsd to typings

--- typings/tsd.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jasmine/jasmine.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="karma-jasmine/karma-jasmine.d.ts" />
-/// <reference path="angularjs/angular-mocks.d.ts" />
-/// <reference path="ng-table/ng-table.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-
