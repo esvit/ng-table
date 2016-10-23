8b28e84284c477962df46fbf756dabcaf793df3d
Christian Crowhurst <ccrowhurst@ramplc.com>
2016-10-23 08:24:06 +0100

    docs(demo-apps): refactor to add app component
    This is an attempt to align with angular 2 code style

---
+++ demo-apps/ts-webpack/src/app.component.ts
@@ -0,0 +1,5 @@
+const templateUrl = require('./app.component.html');
+
+export const appComponent = {
+    templateUrl
+};
--- demo-apps/ts-webpack/src/main.ts
+++ demo-apps/ts-webpack/src/main.ts
@@ -1,7 +1,11 @@
 import * as ng1 from 'angular';
 import { ngTable } from 'ng-table';
+
 import './shared/site.scss'
+
+import { appComponent } from './app.component';
 import { myTableComponent } from './my-table/my-table.component';
 
 ng1.module('demo-app', [ngTable.name])
+    .component('appRoot', appComponent)
     .component('myTable', myTableComponent);
